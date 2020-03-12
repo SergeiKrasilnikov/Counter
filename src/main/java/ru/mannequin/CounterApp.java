83114bd7d208937ea10dc06303a1865c4ff70561
SergeiKrasilnikov <roux-job@mail.ru>
2020-03-12 19:34:30 +0300

    Запуск Boot-приложения

--- src/main/java/ru/mannequin/CounterApp.java
+++ src/main/java/ru/mannequin/CounterApp.java
@@ -1,4 +1,15 @@
 package ru.mannequin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
 public class CounterApp {
+
+    public static void main(String[] args) {
+//        log.debug("Это main");
+        SpringApplication.run(CounterApp.class);
+    }
 }
