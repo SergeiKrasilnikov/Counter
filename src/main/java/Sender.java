119230a18530890cf70a5157d5c16e360443b1f2
aristarchus <s.krasilnikov@r77.center-inform.ru>
2020-03-13 18:13:31 +0300

    отправка через пул потоков

--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class Sender {
@@ -15,7 +17,22 @@ public class Sender {
         sender.createCounterByName("abc");
 //        sender.createCounterByName("def");
 
-        sender.incrementNamedCounter("abc");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    sender.incrementNamedCounter("abc");
+//                    sender.incrementNamedCounter("abc");
+//                    sender.incrementNamedCounter("abc");
+//                    sender.incrementNamedCounter("abc");
+//                    sender.incrementNamedCounter("abc");
+                }
+            });
+
+
+        }
+        sender.getCounterValue("abc");
     }
 
     private void createCounterByName(String counterName) {
@@ -25,7 +42,7 @@ public class Sender {
             HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
             connection.setRequestMethod("PUT");
             int responseCode = connection.getResponseCode();
-            log.debug("При отправке create получен код {}", responseCode);
+//            log.debug("При отправке create получен код {}", responseCode);
             InputStream stream = connection.getInputStream();
             log.debug("Ответ на create: " + IOUtils.toString(stream, StandardCharsets.UTF_8));
         } catch (Exception e) {
@@ -40,9 +57,24 @@ public class Sender {
             HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
             connection.setRequestMethod("POST");
             int responseCode = connection.getResponseCode();
-            log.debug("При отправке increment получен код {}", responseCode);
+//            log.debug("При отправке increment получен код {}", responseCode);
             InputStream stream = connection.getInputStream();
             log.debug("Ответ на increment: " + IOUtils.toString(stream, StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            log.error("Ошибка при инкрементировании счётчика {}.", counterName, e);
+        }
+    }
+
+    private void getCounterValue(String counterName) {
+        log.debug("Отправка запроса на получение значения счётчика {}", counterName);
+        try {
+            URI uri = new URIBuilder("http://localhost:8080/api/value").addParameter("name", counterName).build();
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+//            log.debug("При отправке value получен код {}", responseCode);
+            InputStream stream = connection.getInputStream();
+            log.debug("Ответ на value: " + IOUtils.toString(stream, StandardCharsets.UTF_8));
         } catch (Exception e) {
             log.error("Ошибка при инкрементировании счётчика {}.", counterName, e);
         }
