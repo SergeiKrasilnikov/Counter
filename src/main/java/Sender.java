c9d8290759ea05ea99e2429d9213abea70bcb46e
SergeiKrasilnikov <roux-job@mail.ru>
2020-03-14 08:29:51 +0300

    Удаление счётчика

--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -8,31 +8,51 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Sender {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Sender sender = new Sender();
         sender.createCounterByName("abc");
 //        sender.createCounterByName("def");
 
         ExecutorService executorService = Executors.newCachedThreadPool();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
                     sender.incrementNamedCounter("abc");
-//                    sender.incrementNamedCounter("abc");
-//                    sender.incrementNamedCounter("abc");
-//                    sender.incrementNamedCounter("abc");
-//                    sender.incrementNamedCounter("abc");
+                    sender.incrementNamedCounter("abc");
+                    sender.incrementNamedCounter("abc");
+                    sender.incrementNamedCounter("abc");
+                    sender.incrementNamedCounter("abc");
                 }
             });
-
-
         }
+
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+
         sender.getCounterValue("abc");
+
+        sender.deleteCounter("abc");
+    }
+
+    private void deleteCounter(String counterName) {
+        log.debug("Отправка запроса на удаление счётчика {}.", counterName);
+        try {
+            URI uri = new URIBuilder("http://localhost:8080/api/delete").addParameter("name", counterName).build();
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            connection.setRequestMethod("DELETE");
+            int responseCode = connection.getResponseCode();
+//            log.debug("При отправке delete получен код {}", responseCode);
+            InputStream stream = connection.getInputStream();
+            log.debug("Ответ на delete: " + IOUtils.toString(stream, StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            log.error("Ошибка при обращении к методу delete", e);
+        }
     }
 
     private void createCounterByName(String counterName) {
@@ -46,7 +66,7 @@ public class Sender {
             InputStream stream = connection.getInputStream();
             log.debug("Ответ на create: " + IOUtils.toString(stream, StandardCharsets.UTF_8));
         } catch (Exception e) {
-            log.error("Ошибка при обращении к методу create", e);
+            log.error("Ошибка при создании счётчика", e);
         }
     }
 
@@ -76,7 +96,7 @@ public class Sender {
             InputStream stream = connection.getInputStream();
             log.debug("Ответ на value: " + IOUtils.toString(stream, StandardCharsets.UTF_8));
         } catch (Exception e) {
-            log.error("Ошибка при инкрементировании счётчика {}.", counterName, e);
+            log.error("Ошибка при получении значения счётчика {}.", counterName, e);
         }
     }
 }
